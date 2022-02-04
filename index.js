e1cb6603b95d6e624be088f10c0c8317663f2b33
HLNSCBenWilliams <90395770+HLNSCBenWilliams@users.noreply.github.com>
2022-02-04 12:58:20 +0000

    Add files via upload

--- index.js
+++ index.js
@@ -1,3 +1,30 @@
-function submit() {
-	
+function OpenModal() {
+	// Get the modal
+	var modal = document.getElementById("questionnaire");
+
+	// Get the button that opens the modal
+	var btn = document.getElementById("openModal");
+
+	// Get the <span> element that closes the modal
+	var span = document.getElementById("closeModal");
+
+	// When the user clicks on the button, open the modal
+	modal.style.display = "block";
+
+	// When the user clicks on <span> (x), close the modal
+	span.onclick = function() {
+		modal.style.display = "none";
+	}
+
+	// When the user clicks anywhere outside of the modal, close it
+	window.onclick = function(event) {
+		if (event.target == modal) {
+		modal.style.display = "none";
+		}
+	} 
+}
+
+
+function submitQuestionnaire() {
+	console.log("Submitted Questionnaire")
 }
